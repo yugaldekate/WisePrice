1cddcc4d456fe169f95b1db797df2c62da80f8dd
yugaldekate72 <yugal.dekate@ajatus.co.in>
2023-10-12 23:40:01 +0530

    created carousel for hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image"
 
 import Searchbar from "@/components/Searchbar"
+import HeroCarousel from "@/components/HeroCarousel"
 
 const Home = () => {
   return (
     <>
     
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500" >
+      <section className="px-6 md:px-20 py-24 border-2" >
         <div className="flex max-xl:flex-col gap-16">
 
           <div className="flex flex-col justify-center" >
@@ -32,6 +33,7 @@ const Home = () => {
             <Searchbar/>
           </div>
 
+          <HeroCarousel/>
         </div>
       </section>
 
