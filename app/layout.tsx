2452fb706c113892156d0fdb64c136ddd45a45c7
yugaldekate72 <yugal.dekate@ajatus.co.in>
2023-10-12 15:22:14 +0530

    modified globals.css, tailwind.config, added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter , Space_Grotesk} from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
